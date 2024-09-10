444e34af7358e14e017f342ffec3e6a499039ac5
rohit870891 <koyeb77user@gmail.com>
2024-09-10 10:45:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -86,15 +86,15 @@ UPI_IMAGE_URL = os.environ.get("UPI_IMAGE_URL", "https://t.me/paymentbot6/2")
 SCREENSHOT_URL = os.environ.get("SCREENSHOT_URL", f"t.me/{OWNER_TAG}")
 #Time and its price
 #7 Days
-PRICE1 = os.environ.get("PRICE1", "49 rs")
+PRICE1 = os.environ.get("PRICE1", "20 rs")
 #1 Month
-PRICE2 = os.environ.get("PRICE2", "135 rs")
+PRICE2 = os.environ.get("PRICE2", "49 rs")
 #3 Month
-PRICE3 = os.environ.get("PRICE3", "250 rs")
+PRICE3 = os.environ.get("PRICE3", "135 rs")
 #6 Month
-PRICE4 = os.environ.get("PRICE4", "500 rs")
+PRICE4 = os.environ.get("PRICE4", "250 rs")
 #1 Year
-PRICE5 = os.environ.get("PRICE5", "999 rs")
+PRICE5 = os.environ.get("PRICE5", "500 rs")
 
 
 
