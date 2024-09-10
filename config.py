b4c7a92ba9a7b2fa9ca414ffd2ac85219ef06364
rohit870891 <koyeb77user@gmail.com>
2024-09-10 11:44:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ BOT_STATS_TEXT = os.environ.get("BOTS_STATS_TEXT","<b>BOT UPTIME 🌺</b>\n{upti
 USER_REPLY_TEXT = os.environ.get("USER_REPLY_TEXT", "Don't send me messages directly I'm only File Share bot! 📌")
 
 #your bot token here from https://telegram.me/BotFather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7401282886:AAHdTodD1THJ2nfqnd8ZGo9R76xodImNFl8") 
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7401282886:AAE_HQQRMJpkTMZ8eezjr0euOCLuogo43qI") 
 #your api id from https://my.telegram.org/apps
 APP_ID = int(os.environ.get("APP_ID", "22469064"))
 #your api hash from https://my.telegram.org/apps
